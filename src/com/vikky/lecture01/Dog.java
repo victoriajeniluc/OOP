2d8a5b4eeb8d3c7525065cae10ee7f21fc8ee376
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-26 17:31:03 -0500

    Initial commit

---
+++ src/com/vikky/lecture01/Dog.java
@@ -0,0 +1,20 @@
+package com.vikky.lecture01;
+
+public class Dog {
+    public String name;
+    public int age;
+    public String color;
+    public String breed;
+
+    public void bark() {
+        System.out.println(name + " is barking");
+    }
+
+    public void eating() {
+        System.out.println(name + " is eating");
+    }
+
+    public void wagTail() {
+        System.out.println(name + " is wagging tail");
+    }
+}
