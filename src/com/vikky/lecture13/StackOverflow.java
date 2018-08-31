96a8e2d9cd951432a3f1c2ff588572af22d897fe
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-30 22:32:40 -0500

    create an example of stack overflow

--- src/com/vikky/lecture13/StackOverflow.java
+++ src/com/vikky/lecture13/StackOverflow.java
@@ -8,8 +8,7 @@ package com.vikky.lecture13;
 
 public class StackOverflow {
     public static void main(String [] args) {
-        main(args); // main method is calling itself
-
+        main(args); // main method is calling itself - when this happens.. it keeps pushing the main method on top of the stack(or at least itself)... and there is a stop when the stack memory is all used up... which causes the error of stack overflow to be thrown
         // output:
         /*Exception in thread "main" java.lang.StackOverflowError
         at com.vikky.lecture13.StackOverflow.main(StackOverflow.java:11)
